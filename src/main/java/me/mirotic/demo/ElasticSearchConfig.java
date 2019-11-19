1496c2ed257d70708b03fe76de6a2bd72fc6a6fd
박종욱 <jonguk1221@gmail.com>
2019-11-19 23:55:52 +0900

    config ElasticSearch

---
+++ src/main/java/me/mirotic/demo/ElasticSearchConfig.java
@@ -0,0 +1,45 @@
+package me.mirotic.demo;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Configuration
+@EnableElasticsearchRepositories
+class ElasticSearchConfig {
+
+    @Value("${elasticsearch.cluster-name}")
+    private String clusterName;
+
+    @Value("${elasticsearch.host}")
+    private String host;
+
+    @Value("${elasticsearch.port}")
+    private int port;
+
+    @Bean
+    public Client elasticsearchClient() throws UnknownHostException {
+        Settings settings = Settings.builder().put("cluster.name", this.clusterName).build();
+
+        TransportClient client = new PreBuiltTransportClient(settings);
+        client.addTransportAddress(new TransportAddress(InetAddress.getByName(this.host), this.port));
+        return client;
+    }
+
+    @Bean
+    public ElasticsearchOperations elasticsearchTemplate() throws UnknownHostException {
+        return new ElasticsearchTemplate(elasticsearchClient());
+    }
+
+}
